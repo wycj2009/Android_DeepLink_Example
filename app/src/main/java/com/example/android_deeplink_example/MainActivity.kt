74a56bccb2dca08068d9fb89723d92b7095bc8b0
wycj2009 <wycj2009@naver.com>
2023-09-01 16:28:58 +0900

    Updated

--- app/src/main/java/com/example/android_deeplink_example/MainActivity.kt
+++ app/src/main/java/com/example/android_deeplink_example/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android_deeplink_example
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_deeplink_example.databinding.ActivityMainBinding
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val uri: Uri = intent.data ?: return finish()
+
+        // Sample uri scheme - testapp://main?a=1&b=2
+        binding.text.text = buildString {
+            appendLine("uri = $uri")
+            appendLine("a = ${uri.getQueryParameter("a")}")
+            appendLine("b = ${uri.getQueryParameter("b")}")
+        }
     }
 }
